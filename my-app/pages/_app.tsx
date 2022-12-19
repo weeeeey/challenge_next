073b44ada019938fb045f2905f7cb41bcc196762
weeeeey <qser155@naver.com>
2022-12-20 03:08:51 +0900

    #첫번째 페이지

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -1,6 +1,10 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <div className="mt-10 max-w-xl mx-auto w-full h-[100vh]">
+            <Component {...pageProps} />
+        </div>
+    );
 }
--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,46 +1,121 @@
-import axios from 'axios';
 import { NextPage } from 'next';
-import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
 
-interface Person {
-    id: string;
-    industries: string[];
-    name: string;
-    netWorth: number;
-    squareImage: string;
-}
 const Index: NextPage = () => {
-    const [data, setData] = useState<Person[]>();
-    const router = useRouter();
-    useEffect(() => {
-        (async () => console.log('innerFunction'))();
-    });
-    useEffect(() => {
-        (async () => {
-            axios
-                .get('https://billions-api.nomadcoders.workers.dev/')
-                .then((res) => setData(res.data));
-        })();
-    }, []);
     return (
-        <div className="w-full h-full bg-slate-700 grid grid-cols-4">
-            {data?.map((person: Person) => (
-                <div key={person.id} className="w-52 h-60 m-3 bg-red-300">
-                    <div className="w-full">
-                        <img src={person.squareImage} alt={person.name} />
+        <div className="flex-row justify-center items-center mx-4">
+            <h1 className="text-5xl mb-10 font-bold">Weather</h1>
+            <div className="space-y-5 mb-10">
+                <div className=" w-full h-20 border-2 border-black shadow-md border-b-4 bg-white text-black rounded-xl flex justify-between items-center ">
+                    <div className="ml-4 flex-col justify-center items-center">
+                        <div className="font-semibold text-2xl">Casius</div>
+                        <div className="font-medium text-sm">Mars, 12AM</div>
                     </div>
-                    <div className="flex-col-reverse shadow-md bg-slate-700 ">
-                        <div className="text-white text-sm">{person.name}</div>
-                        <div className="text-slate-300 text-xs ">
-                            <span>
-                                {Math.round(person.netWorth / 1000)}billion
-                            </span>
-                            /<span>{person.industries[0]}</span>
-                        </div>
+                    <div className="mr-4 flex items-center">
+                        <svg
+                            className="w-full h-12"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                            />
+                        </svg>
+                        <div className="font-bold text-4xl"> 85&#186;</div>
                     </div>
                 </div>
-            ))}
+                <div className=" w-full h-20 border-2 border-black shadow-md border-b-4 bg-yellow-400 text-black rounded-xl flex justify-between items-center ">
+                    <div className="ml-4 flex-col justify-center items-center">
+                        <div className="font-semibold text-2xl">Casius</div>
+                        <div className="font-medium text-sm">Mars, 12AM</div>
+                    </div>
+                    <div className="mr-4 flex items-center">
+                        <svg
+                            className="w-full h-12"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                            />
+                        </svg>
+                        <div className="font-bold text-4xl"> 85&#186;</div>
+                    </div>
+                </div>
+                <div className=" w-full h-20 border-2 border-black shadow-md border-b-4 bg-green-400 text-black rounded-xl flex justify-between items-center ">
+                    <div className="ml-4 flex-col justify-center items-center">
+                        <div className="font-semibold text-2xl">Casius</div>
+                        <div className="font-medium text-sm">Mars, 12AM</div>
+                    </div>
+                    <div className="mr-4 flex items-center">
+                        <svg
+                            className="w-full h-12"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                            />
+                        </svg>
+                        <div className="font-bold text-4xl"> 85&#186;</div>
+                    </div>
+                </div>
+                <div className=" w-full h-20 border-2 border-black shadow-md border-b-4 bg-orange-600 text-black rounded-xl flex justify-between items-center ">
+                    <div className="ml-4 flex-col justify-center items-center">
+                        <div className="font-semibold text-2xl">Casius</div>
+                        <div className="font-medium text-sm">Mars, 12AM</div>
+                    </div>
+                    <div className="mr-4 flex items-center">
+                        <svg
+                            className="w-full h-12"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                            />
+                        </svg>
+                        <div className="font-bold text-4xl"> 85&#186;</div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <button>
+                    <svg
+                        className="w-16 h-16"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={1}
+                            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                    </svg>
+                </button>
+            </div>
         </div>
     );
 };
