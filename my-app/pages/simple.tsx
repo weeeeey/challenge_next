334e6b55134dae2c49c8799ed4b85174c707cf70
weeeeey <qser155@naver.com>
2022-12-20 03:39:37 +0900

    #두번째 페이지

---
+++ my-app/pages/simple.tsx
@@ -0,0 +1,104 @@
+import { NextPage } from 'next';
+
+const SimpleTag: NextPage = () => {
+    return (
+        <div className="mx-auto w-full">
+            <div>
+                <svg
+                    className="w-12 h-12"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={1}
+                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                </svg>
+            </div>
+            <div className="flex flex-col items-center mb-8 ">
+                <div className="font-semibold text-lg text-gray-500 pb-4">
+                    SIMPLE TAG
+                </div>
+                <div className="flex flex-col items-center text-5xl font-bold">
+                    <div>Work with best</div>
+                    <div>designer</div>
+                </div>
+            </div>
+            <div className="w-full h-96 grid grid-cols-2 grid-rows-2 gap-4 mb-8">
+                <div className="border-2 border-black w-full h-full rounded-2xl bg-yellow-400 flex justify-center items-center ">
+                    <svg
+                        className="w-24 h-24 text-white"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={0.5}
+                            d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                        />
+                    </svg>
+                </div>
+                <div className="border-2 border-black w-full h-full rounded-2xl bg-green-400 flex justify-center items-center ">
+                    <svg
+                        className="w-24 h-24 text-white"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={0.5}
+                            d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                        />
+                    </svg>
+                </div>
+                <div className="border-2 border-black w-full h-full rounded-2xl bg-pink-400 flex justify-center items-center ">
+                    <svg
+                        className="w-24 h-24 text-white"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={0.5}
+                            d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                        />
+                    </svg>
+                </div>
+                <div className="border-2 border-black w-full h-full rounded-2xl bg-orange-600 flex justify-center items-center ">
+                    <svg
+                        className="w-24 h-24 text-white"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={0.5}
+                            d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                        />
+                    </svg>
+                </div>
+            </div>
+            <button className="w-full h-14 rounded-2xl text-xl font-semibold text-white bg-blue-600">
+                Let&#39;s get it done
+            </button>
+        </div>
+    );
+};
+
+export default SimpleTag;
