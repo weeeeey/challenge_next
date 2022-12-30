55bb511688d563b0d036dfc0887b50e10b4ce187
weeeeey <qser155@naver.com>
2022-12-30 12:17:19 +0900

    첼린지9) 최종 제출

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,47 +1,44 @@
 import { NextPage } from "next";
-import { useForm } from "react-hook-form";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import useDog from "../libs/useDog";
 
 const Index: NextPage = () => {
+    const [isLike, setIsLike] = useState<boolean>(false);
     const videoRef = useRef();
-    const { handleSubmit } = useForm();
     const { mutate, dog, isLoading } = useDog();
-    const onClickNewDog = () => {
-        mutate();
+    const onClickNewDog = (e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+        mutate((prev: any) => ({ ...prev, isLiked: isLike }), false);
         console.log(dog);
     };
-    const onClieckLike = () => {
-        mutate((prev: any) => ({ ...prev, isLiked: !prev.isLiked }), false);
+    const onClieckLike = (e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+        setIsLike((prev) => !prev);
     };
-    const onValid = () => {
-        console.log("as");
-    };
+
     useEffect(() => {
         videoRef.current?.load();
-    }, [dog, mutate]);
+        setIsLike((prev) => !prev);
+    }, [dog?.url]);
     return (
         <div className="w-full max-w-xl mx-auto h-screen">
             {dog?.url ? (
                 <>
                     <div className="w-full mx-auto">
                         <h1 className="pt-4 text-3xl text-white ">Wolf.tv</h1>
-                        <form
-                            onSubmit={handleSubmit(onValid)}
-                            className="flex flex-col items-center justify-center"
+                        {/* <div className="m-2 w-full h-44 bg-slate-300"></div> */}
+
+                        <video
+                            ref={videoRef}
+                            muted
+                            autoPlay
+                            loop
+                            className="aspect-auto h-96 bg-black mx-auto"
                         >
-                            {/* <div className="m-2 w-full h-44 bg-slate-300"></div> */}
+                            <source src={dog.url} />
+                        </video>
 
-                            <video
-                                ref={videoRef}
-                                muted
-                                autoPlay
-                                loop
-                                className="aspect-auto h-96 bg-black"
-                            >
-                                <source src={dog.url} />
-                            </video>
-
+                        <form className="flex flex-col items-center justify-center">
                             <div className="mt-4 w-full flex items-center space-x-3">
                                 <button
                                     onClick={onClickNewDog}
@@ -51,9 +48,9 @@ const Index: NextPage = () => {
                                 </button>
                                 <button
                                     onClick={onClieckLike}
-                                    className="h-12 rounded-xl  w-full bg-blue-400"
+                                    className="h-12 rounded-xl  w-full bg-blue-400 text-white"
                                 >
-                                    {dog.isLiked ? "unLike" : "Like"}
+                                    {isLike ? "Unlike" : "like"}
                                 </button>
                             </div>
                         </form>
